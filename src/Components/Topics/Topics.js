e19975cdf9aab52f0193547d8007d57d850aacbb
abujayed007 <abujayed007@gmail.com>
2022-10-12 16:40:48 +0600

    final commit

--- src/Components/Topics/Topics.js
+++ src/Components/Topics/Topics.js
@@ -9,7 +9,7 @@ const Topics = ({quiz}) => {
             <img className='pt-2' style={{width:'300px'}} src={logo} alt="" />
            <div className="d-flex justify-content-around mt-5 mb-3">
            <h2 className='text-success'>{name}</h2>
-            <Link to={`practice/${id}`}> <button className='border-0 p-2 bg-info rounded fw-bold'>Start Practice</button> </Link>
+            <Link to={`practice/${id}`}> <button className='border-0 text-white p-2 bg-info rounded fw-bold'>Start Quiz</button> </Link>
            </div>
         </div>
     );
